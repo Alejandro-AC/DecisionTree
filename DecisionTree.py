6be6484eb49f20ce68092bd8999356862ab60804
Ronnyrom <ronnyrom3@gmail.com>
2019-11-06 12:56:32 +0100

    Leer archivo2

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,12 +1,17 @@
 
+import pprint
 
 
 
 
 if __name__ == "__main__":
-    dirFichero = 'agaricus-lepiota.data'
+    dirFichero = 'data/agaricus-lepiota.data'
     with open(dirFichero, 'r') as reader:
+        lista= []
+        
         for line in reader:
-            for elemento in line.split(","):
-                print elemento
-                
+            listaux=[] 
+            for elemento in line.replace("\n","").split(","):
+                listaux.append(elemento)    
+            lista.append(listaux)        
+        pprint.pprint(lista[8900][0])
