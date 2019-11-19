58d3ef2f06128fcba6bec668acbea9ebfb82e19c
Alejandro Aznar <a.aznarcortes@gmail.com>
2019-11-19 21:56:57 +0100

    Clean decisionAlgorithm
    Remove duplicated code and format

--- DecisionAlgorithm.py
+++ DecisionAlgorithm.py
@@ -41,13 +41,11 @@ class DecisionAlgorithm:
         return max_gain_ratio_attribute_idx
 
     def decide_gini(self, data_set):
-        #base_entropy = self.calculate_entropy(data_set[:, 0])
-        #attribute_gini_list = self.calculate_gini(base_entropy, data_set)
         base_gini = self.calculate_gini(data_set[:, 0])
 
-        attribute_gini_gain_list = self.calculate_gini_gain(base_gini, data_set)
+        attribute_gini_gain_list = self.calculate_gini(base_gini, data_set, gini=True)
         max_gini_gain_attribute_idx = np.argmax(
-        attribute_gini_gain_list[1:]) + 1  # Exclude first since it's the class
+            attribute_gini_gain_list[1:]) + 1  # Exclude first since it's the class
 
         return max_gini_gain_attribute_idx
 
@@ -61,61 +59,49 @@ class DecisionAlgorithm:
 
         return -(norm_counts * np.log(norm_counts) / np.log(base)).sum()
 
-    def calculate_gain(self, base_entropy=None, data_set=None):
+    def calculate_gain(self, base=None, data_set=None, gini=None):
         data_set = self.data_set if data_set is None else data_set
+        gini = False if gini is None else gini
 
         num_columns = data_set.shape[1]
-        gain_list = [base_entropy] * num_columns  # Initialize with base value
+        gain_list = [base] * num_columns  # Initialize with base value
         for col in range(1, num_columns):
             unique_values, counts = np.unique(data_set[:, col], return_counts=True)
             total_count = counts.sum()
             reduced_matrix = data_set[:, [0, col]]  # Matrix with only class and current columns
             for value, count in zip(unique_values, counts):
                 value_rows = reduced_matrix[np.where(reduced_matrix[:, 1] == value)]  # Matrix of rows with current val
-                gain_list[col] -= count / total_count * self.calculate_entropy(value_rows[:, 0], 2)
-                
+                if gini:
+                    gain_list[col] -= count / total_count * self.calculate_gini(value_rows[:, 0])
+                else:
+                    gain_list[col] -= count / total_count * self.calculate_entropy(value_rows[:, 0], 2)
 
         return gain_list
 
     def calculate_split_info(self, base_entropy, data_set):
-        
+
         split_info_list = [0] * data_set.shape[1]
         num_columns = data_set.shape[1]
-        for col in range(1,num_columns):
+        for col in range(1, num_columns):
             unique_values, counts = np.unique(data_set[:, col], return_counts=True)
             total_count = counts.sum()
             for value, count in zip(unique_values, counts):
-                split_info_list[col]-=(count/total_count)*np.log(count/total_count) / np.log(2)
+                split_info_list[col] -= (count / total_count) * np.log(count / total_count) / np.log(2)
 
         return split_info_list
 
     def calculate_gain_ratio(self, gain_list, split_info_list):
-        gain_ratio=[0]*len(gain_list)
-        for col in range(1,len(gain_list)):
-            if split_info_list[col]!=0:
-                gain_ratio[col]=gain_list[col]/split_info_list[col]  
+        gain_ratio = [0] * len(gain_list)
+        for col in range(1, len(gain_list)):
+            if split_info_list[col] != 0:
+                gain_ratio[col] = gain_list[col] / split_info_list[col]
 
         return gain_ratio
 
-    def calculate_gini(self,  class_values=None, base=None):
-         class_values = self.data_set[:, 0] if class_values is None else class_values
-         value, counts = np.unique(class_values, return_counts=True)
-         norm_counts = counts / counts.sum()
-                  
+    def calculate_gini(self, class_values=None):
+        class_values = self.data_set[:, 0] if class_values is None else class_values
+        value, counts = np.unique(class_values, return_counts=True)
+        norm_counts = counts / counts.sum()
 
-         return 1 - (norm_counts**2).sum()
+        return 1 - (norm_counts ** 2).sum()
 
-    def calculate_gini_gain(self, base_entropy=None, data_set=None):
-        data_set = self.data_set if data_set is None else data_set
-
-        num_columns = data_set.shape[1]
-        gain_list = [base_entropy] * num_columns  # Initialize with base value
-        for col in range(1, num_columns):
-            unique_values, counts = np.unique(data_set[:, col], return_counts=True)
-            total_count = counts.sum()
-            reduced_matrix = data_set[:, [0, col]]  # Matrix with only class and current columns
-            for value, count in zip(unique_values, counts):
-                value_rows = reduced_matrix[np.where(reduced_matrix[:, 1] == value)]  # Matrix of rows with current val
-                gain_list[col] -= count / total_count * self.calculate_gini(value_rows[:, 0], 2)
-
-        return []
