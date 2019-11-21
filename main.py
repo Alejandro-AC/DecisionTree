89d01d572bb3e715c1e96e6239e00a7368ba74d3
Alejandro Aznar <a.aznarcortes@gmail.com>
2019-11-21 01:54:20 +0100

    Add executable

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Created on Thu Nov  7 17:56:23 2019
 '''
 
 import DataSet as ds
+import argparse
 import Evaluation as ev
 import numpy as np
 
@@ -69,5 +70,64 @@ if __name__ == '__main__':
     # Decision algorithms: - C45 - ID3 - Gini
     # Evaluation methods: - Holdout - Cross-Validation - Bootstrap
 
-    eval = ev.Evaluation(dirFichero, 'C45', 'Holdout', k=None, leave_one_out=None, training_set_percentage=None,
-                         advanced_missing_values=None)
+    # Initialize the parser
+    parser = argparse.ArgumentParser(
+        description="DataSet evaluation with DecisionAlgorithms"
+    )
+
+    parser.add_argument('k', help="number of iterations for Cross-Validation or Bootstrap evaluation methods", type=int)
+    parser.add_argument('trainpercentage', help="Percentage of the dataset destined to the training subset", type=float)
+
+    parser.add_argument("-a", help="Use Advanced missing values processing", action="store_true")
+    parser.add_argument("-l",
+                        help="Use Leave one out with Cross-Validation Evaluation method (may take several minutes to complete)")
+
+    parser.add_argument("-c", help="Use C4.5 Decision Algorithm", action="store_true")
+    parser.add_argument("-i", help="Use ID3 Decision Algorithm", action="store_true")
+    parser.add_argument("-g", help="Use Gini Decision Algorithm", action="store_true")
+
+    parser.add_argument("-h", help="Use Holdout Evaluation method", action="store_true")
+    parser.add_argument("-v", help="Use Cross-Validation Evaluation method", action="store_true")
+    parser.add_argument("-b", help="Use Bootstrap Evaluation method", action="store_true")
+
+    # Parse the arguments
+    arguments = parser.parse_args()
+
+    if arguments:
+        advanced_missing_values = False
+        leave_one_out = False
+        training_set_percentage = arguments.trainpercentage
+        k = arguments.k
+        evaluation_method = 'Holdout'
+        decision_algorithm = 'ID3'
+
+        print("\n")
+        if arguments.a:
+            print("Using Advanced missing values processing")
+            advanced_missing_values = True
+
+        if arguments.l:
+            print("Using Leave one Out evaluation")
+            leave_one_out = True
+
+        if arguments.c:
+            decision_algorithm = 'C45'
+        elif arguments.i:
+            decision_algorithm = 'ID3'
+        elif arguments.g:
+            decision_algorithm = 'Gini'
+
+        print('Decision algorithm used: ' + decision_algorithm)
+
+        if arguments.h:
+            evaluation_method = 'Holdout'
+        elif arguments.v:
+            evaluation_method = 'Cross-Validation'
+        elif arguments.b:
+            evaluation_method = 'Bootstrap'
+
+        print('Evaluation method used: ' + evaluation_method)
+
+        ev.Evaluation(dirFichero, decision_algorithm, evaluation_method, k, leave_one_out,
+                      training_set_percentage,
+                      advanced_missing_values)
